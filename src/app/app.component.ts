d79af586b3fd082cd2fe32fc93271abbe068e011
Juan Camilo Valdes Bolivar <bolivar014@gmail.com>
2022-09-04 13:31:08 -0500

    usando ngDestroy y setTimeOut en inputs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,8 @@ import { Product } from './models/product.model';
 export class AppComponent {
   imgParent = 'https://i.pinimg.com/564x/b5/26/44/b5264448622d0a2536067610dc91b3c4.jpg';
 
+  showImg = true;
+
   // Objeto products
   products: Product[] = [
     {
@@ -39,5 +41,10 @@ export class AppComponent {
   // Evento loaded del padre
   onLoaded(img: string) {
     console.log('log padre', img);
+  }
+
+  // Cambiamos estado de imagen
+  toggleImg() {
+    this.showImg = !this.showImg;
   }
 }
--- src/app/components/img/img.component.ts
+++ src/app/components/img/img.component.ts
@@ -1,20 +1,46 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, OnChanges, AfterViewInit, OnDestroy, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-img',
   templateUrl: './img.component.html',
   styleUrls: ['./img.component.css']
 })
-export class ImgComponent implements OnInit {
+export class ImgComponent implements OnInit, OnChanges, AfterViewInit, OnDestroy {
+  img: string = "";
+  @Input('img')
+  set changeImg(newImg: string) {
+    this.img = newImg;
+    console.log('change just img', this.img);
+  }
 
-  @Input() img: string = "Valor inicial";
+  @Input() alt: string = "";
   // Envio imagen desde componente hijo hacía padre
   @Output() loaded = new EventEmitter<string>();
 
   imageDefault = "https://pm1.narvii.com/7124/83568d283df6c34715a1cf898dae00ca5b57163ar1-720-1280v2_hq.jpg";
-  constructor() { }
+  counter = 0;
+  counterFn: number | undefined;
 
+  constructor() {
+    // Before Render
+    // No Async
+    console.log('se ha ejecutado el constructor', 'imgValue => ', this.img);
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    // Before Render
+    // changes inputs - times
+    console.log('ngOnChanges', 'imgValue => ', this.img);
+    console.log('changes:', changes);
+  }
   ngOnInit(): void {
+    // Before Render
+    // Async - Fetch - promises - Once Time
+    console.log('ngOnChanges', 'imgValue => ', this.img);
+    this.counterFn = window.setInterval(() => {
+      this.counter += 1;
+      console.log('counter: ', this.counter);
+    }, 1000)
   }
 
   // Evento para control de error - cuando no existe imagen
@@ -27,5 +53,18 @@ export class ImgComponent implements OnInit {
     console.log('loaded hijo');
     // Compartimos string de la imagen
     this.loaded.emit(this.img);
+  }
+
+  ngAfterViewInit(): void {
+    // After Render
+    // handler children
+    console.log('ngAfterViewInit');
+  }
+
+  ngOnDestroy(): void {
+    // Delete
+    console.log('ngOnDestroy');
+    // Limpiamos timer
+    window.clearInterval(this.counterFn);
   }
 }
